a4270cceb1ccd6083474e2db2a8079ccc2475cde
Justin Chines <justin.chines@gmail.com>
2014-05-13 18:48:59 -0400

    Added topic updating support. Added jakeism spouting.

--- jakebot.rb
+++ jakebot.rb
@@ -3,9 +3,13 @@ require 'twitter'
 require 'yaml'
 
 affirmatives = [ "Got it!", "Word.", "For sure, yo.", "Fo shizzle." ]
-greetings = [ "What up.", "Yo!", "The funk has arrived." ]
+welcomes = [ "What up.", "Yo!", "The funk has arrived." ]
+greetings = [ "Hello", "What up", "Hi", "How you been", "How ya doin", "Howdy" ]
+jakeisms = [ "So how has everyone's day been?", "beep boop", "RBE is the best. You guys are just jealous", "I could sure go for some Pi right now...", "miku es mai waifu", "I love Spanish! Quiero un mexicano que me jodas en el culo!" ]
+
 bot_dir = File.expand_path "~/.jakebot"
 welcome_messages = {}
+channels = ["#bottest"]
 
 client = Twitter::REST::Client.new do |config|
   config.consumer_key = "IOo4mv0KW65QOVh4nUYApEdML"
@@ -15,14 +19,18 @@ client = Twitter::REST::Client.new do |config|
 end
 
 bot = Cinch::Bot.new do
+  # Configure the bot
+
   configure do |c|
     c.server = "irc.phinugamma.org"
-    c.channels = ["#bottest"]
+    c.channels = channels
     c.nick = "jakebot"
   end
 
-  on :message, "hello jakebot" do |m|
-    m.reply "hi #{m.user.nick}"
+  # Register handlers
+
+  on :message, /^(hello|hi) jakebot/ do |m|
+    m.reply "#{greetings.sample} #{m.user.nick}"
   end
 
   on :message, /^!tweet (.+)/ do |m, tw|
@@ -48,16 +56,56 @@ bot = Cinch::Bot.new do
     # Case of bot joining
     if m.user == bot.nick
       m.reply "HELLO EVERYONE! I AM JAKEBOT v0.2"
-      return
-    end
-
-    m.channel.op(m.user)
-    if welcome_messages.key?(m.user.nick)
-      m.reply welcome_messages[m.user.nick]
     else
-      m.reply greetings.sample
+      m.channel.op(m.user)
+      if welcome_messages.key?(m.user.nick)
+        m.reply welcome_messages[m.user.nick]
+      else
+        m.reply welcomes.sample
+      end
     end
   end
+
+  on :message, /^!topic ?add (.+)/ do |m, new_topic|
+    current_topic = m.channel.topic
+    if current_topic.empty? 
+      m.channel.topic = new_topic
+    else
+      m.channel.topic = "#{current_topic} | #{new_topic}"
+    end
+  end
+
+  on :message, /^!topic ?rem(ove)? (.+)/ do |m, garbage, top|
+    # Remove an items from the topic
+    
+    reg = Regexp.new(top, true) # Case insensitive regexp
+    current_topic = m.channel.topic
+    topic_segments = current_topic.split(" | ")
+    
+    topic_segments.each do |t|
+      if reg =~ t
+        topic_segments.delete(t)
+      end
+    end
+    
+    new_topic = topic_segments.join(" | ")
+    
+    if new_topic.eql? current_topic
+      # If the edited topic and new topic are the same, the
+      # requested item to delete must not have been found
+      m.reply "That's not in the topic" 
+    else
+      m.channel.topic = new_topic
+    end
+  end
+
+  # Start timers
+
+  Timer(10 * 60) { # Every 10 minutes
+    if rand < 0.25 # 25% chance
+      channels.each do |chan| Channel(chan).send(jakeisms.sample) end
+    end
+  }
 end
 
 # Create the storage directory if it doesn't exist
@@ -70,5 +118,3 @@ end
 
 # Start the bot
 bot.start
-
-
